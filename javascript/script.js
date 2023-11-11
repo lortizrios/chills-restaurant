ced65c343d602fc6b7f1b5c690d8184a6fbea210
Leroy Ortiz Rios <lortiz8984@interbayamon.edu>
2023-11-11 19:03:09 -0400

    Just add some folders

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+const burgerIcon= document.querySelector('#burger');
+
+const navbarMenu=document.querySelector("#nav-links");
+
+burgerIcon.addEventListener('click',()=> {
+    navbarMenu.classList.toggle('is-active')
+})
+
+
+
+
+
+// 
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  } 
