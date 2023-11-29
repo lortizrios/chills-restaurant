5b819e36572c3ca8c8f0947aa9847255e4fcffeb
Jose Rivera <josea12134@outlook.com>
2023-11-28 21:20:09 -0400

    Arreglo typo

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <title>Login</title>
     <link rel="stylesheet" href="bulma/css/bulma.css">
     <link rel="stylesheet" href="css/mystyles.css">
+    
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
