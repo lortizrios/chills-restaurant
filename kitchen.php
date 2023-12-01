2cdfa491dfb3a753b421de94be404813b1a5ebc2
Jose Rivera <josea12134@outlook.com>
2023-11-30 21:00:04 -0400

    Kitchen y NavSnow

---
+++ kitchen.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <title>Three Columns Layout</title>
+</head>
+<body>
+<section class="section">
+    <div class="container">
+        <h1 class="title">Kitchen</h1>
+        <div class="columns">
+            <!-- First Column -->
+            <div class="column">
+                <p class="has-background-info has-text-white p-4">
+                    Orders:
+                </p>
+                <div class="column">
+                    <p>Place Holder</p>
+                </div>
+                
+            </div>
+
+            <!-- Second Column -->
+            <div class="column">
+                <p class="has-background-success has-text-white p-4">
+                    This is the second column.
+                    This is the second column.
+                    This is the second column.
+                    This is the second column.
+                </p>
+            </div>
+            <!-- Third Column -->
+            <div class="column">
+                <p class="has-background-warning has-text-white p-4">
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.
+                    This is the third column.This is the third column.
+                    This is the third column.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
